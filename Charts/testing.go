de7e3687791832fce38df5970a01f80d5fb08e51
Rehmanareeb <rehmanareeb24@gmail.com>
2023-10-26 00:24:09 +0500

    added support for user provided URL

--- Charts/testing.go
+++ Charts/testing.go
@@ -10,8 +10,6 @@ func Get_The_Name_OfWebsite() string {
 
 	fmt.Println("Enter the name of the Website on which you want to perform the response test: ")
 	fmt.Scan(&URL)
-	// Remove the newline character from the URL
-	//URL = strings.TrimSuffix(URL, "\n")
 
 	// Check if the URL has .com in it
 	if !strings.HasSuffix(URL, ".com") {
